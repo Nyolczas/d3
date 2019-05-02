347be262f2504c481ea349107288cc27f233e1e2
Nyolczas <nyolczas@gmail.com>
2019-05-02 22:31:19 +0200

    d3 barchart

--- js/barchart.js
+++ js/barchart.js
@@ -1,17 +1,18 @@
 var dataset = [25, 24, 29, 36, 32, 31, 25, 28, 35, 43, 42, 58];
 
+// méretek meghatározása
 var maxVal = Math.max(...dataset);
-var topPad = maxVal * 0.1;
+var topPadding = maxVal * 0.2;
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
 
-var maxBarHeight = svgHeight - topPad;
-var multiplying = maxBarHeight / maxVal;
+var maxBarHeight = svgHeight - topPadding;
+var dataMultiplying = maxBarHeight / maxVal;
 
-d3.select('body').append('p').text(multiplying); 
+d3.select('body').append('p').text(dataMultiplying); 
 
-// svg konténer kijelölése és méret beállítása
+// svg konténer kijelölése és beállítása
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -21,13 +22,34 @@ var barChart = svg.selectAll("rect")
     .enter()
     .append("rect")
     .attr("y", function(d) {
-        return svgHeight - d * multiplying;
+        return svgHeight - d * dataMultiplying;
     })
     .attr("height", function(d) {
-        return d * multiplying;
+        return d * dataMultiplying;
     })
     .attr("width", barWidth - barPadding)
+    .attr("class", "bar")
     .attr("transform", function (d, i) {
         var translate = [barWidth * i, 0];
         return "translate(" + translate + ")";
-    });
+    })
+    ;
+
+// datalabels hozzáadása
+
+var text= svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("y", function(d, i) {
+        return svgHeight - d * dataMultiplying -2;
+    })
+    .attr("x", function(d,i) {
+        return barWidth * i + (barWidth - barPadding) * 0.5;
+    })
+    .attr("text-anchor", "middle")
+    .attr("class", "dataLabels");
+
